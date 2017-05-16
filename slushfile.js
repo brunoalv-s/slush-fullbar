ef605a8f7ed581b4880de83a15193ad58ad4ed61
Bruno Alves <brunoalv.s@outlook.com>
2017-05-16 16:07:05 -0300

    escolhas não funcionando

--- slushfile.js
+++ slushfile.js
@@ -1,33 +1,40 @@
 /*
- * slush-goku
- * https://github.com/seu-nome/slush-goku
+ * slush-fullbar
+ * https://github.com/seu-nome/slush-fullbar
  *
- * Copyright (c) 2014, Seu Nome
+ * Copyright (c) 2014, Fullbar
  * Licensed under the MIT license.
  */
 
 'use strict';
 
 var gulp     = require('gulp'),
-    install  = require('gulp-install'),
     conflict = require('gulp-conflict'),
-    template = require('gulp-template'),
+    filter   = require('gulp-filter'),
+    install  = require('gulp-install'),
     rename   = require('gulp-rename'),
-    _        = require('underscore.string'),
-    inquirer = require('inquirer');
+    template = require('gulp-template'),
+    gutil    = require('gulp-util'),
+    inquirer = require('inquirer'),
+    _        = require('underscore.string');
+
+    var paths = {
+      frameworkCSS: {
+        bootstrap: '/assets/sass/1-settings/vendor/bootstrap/**',
+        bulma: '/assets/sass/1-settings/vendor/bulma/**/*'
+      },
+      templateEngine: {
+        pug: '/**/*.pug',
+        html: '/**/*.html'
+      }
+    };
+
+    var options = {
+      templatesDir: __dirname + '/templates',
+    };
+
 
 gulp.task('default', function(done) {
-
-  var options = {
-    templatesDir: __dirname + '/templates',
-  };
-
-  var paths = {
-    cssFramework: {
-      bootstrap: 'sass/1-settings/vendor/bootstrap/**',
-      bulma: 'sass/1-settings/vendor/bulma/**'
-    }
-  }
 
   // Lista de Perguntas
   var prompts = [{
@@ -42,7 +49,7 @@ gulp.task('default', function(done) {
       type: 'checkbox',
       name: 'cssFramework',
       message: 'Qual Framework CSS você vai usar?',
-      choices: ['boostrap', 'foundation-sites', 'bulma', 'Nenhum'],
+      choices: ['boostrap', 'bulma', 'Nenhum'],
       default: 'Nenhum'
   }, {
       type: 'checkbox',
@@ -56,60 +63,56 @@ gulp.task('default', function(done) {
     message: 'Continuar?'
   }];
 
-
+  //Ask
   inquirer.prompt(prompts, function(answers) {
-    if (!answers.moveon) {
+    if (!answers.appName) {
       return done();
     }
 
-    answers.projectNameSlug = _.slugify(answers.projectName);
-    var d = new Date();
-    answers.year = d.getFullYear();
-
+    answers.appNameSlug = _.slugify(answers.appName)
+    answers.appDescriptionSlug = _.slugify(answers.appDescription)
     var files = [options.templatesDir + '/**'];
     if (answers.cssFramework === 'bootstrap') {
       files.push(
-        '!' + options.templatesDir + 'caminho/foundation-sites',
-        '!' + options.templatesDir + paths.cssFramework.bulma
+        '!' + options.templatesDir + paths.frameworkCSS.bulma
       );
     } else if (answers.cssFramework === 'bulma') {
       files.push(
-        '!' + options.templatesDir + paths.cssFramework.bootstrap,
-        '!' + options.templatesDir + 'caminho/foundation-sites'
+        '!' + options.templatesDir + paths.frameworkCSS.bootstrap
       );
-    } else if (answers.cssFramework === 'foundation-sites') {
+    } else if (answers.cssFramework === 'Nenhum') {
       files.push(
-        '!' + options.templatesDir + paths.cssFramework.bootstrap,
-        '!' + options.templatesDir + paths.cssFramework.bulma
-      );
-    } else {
-      files.push(
-        '!' + options.templatesDir + paths.cssFramework.bootstrap,
-        '!' + options.templatesDir + paths.cssFramework.bulma,
-        '!' + options.templatesDir + 'caminho/foundation-sites'
+        '!' + options.templatesDir + paths.frameworkCSS.bootstrap,
+        '!' + options.templatesDir + paths.frameworkCSS.bulma
       );
     };
+
+    if (answers.templateEngine === 'Nenhum') {
+      files.push(
+        '!' + options.templatesDir + paths.templateEngine.pug
+      )
+    } else if (answers.templateEngine === 'jade/pug') {
+      files.push(
+        '!' + options.templatesDir + paths.templateEngine.html
+      );
+    };
+
+    var filterImagesAndFonts = filter(['**/**', '!assets/fonts/**', '!assets/img/**'], {
+      restore: true
+    });
+
+    gulp.src(files)
+      .pipe(template(answers))
+      .pipe(rename(function(file) {
+        if (file.basename[0] === '_') {
+          file.basename = '.' + file.basename.slice(1);
+        }
+      }))
+      .pipe(conflict('./'))
+      .pipe(gulp.dest('./'))
+      .pipe(install())
+      .on('end', function() {
+        done();
+      });
   });
-    //Ask
-    // inquirer.prompt(prompts,
-    //     function(answers) {
-    //         if (!answers.appName) {
-    //             return done();
-    //         }
-    //         answers.appNameSlug = _.slugify(answers.appName)
-    //         answers.appAuthorSlug = _.slugify(answers.appAuthor)
-    //         gulp.src(__dirname + '/templates/**')
-    //             .pipe(template(answers))
-    //             .pipe(rename(function(file) {
-    //                 if (file.basename[0] === '_') {
-    //                     file.basename = '.' + file.basename.slice(1);
-    //                 }
-    //             }))
-    //             .pipe(conflict('./'))
-    //             .pipe(gulp.dest('./'))
-    //             .pipe(install())
-    //             .on('end', function() {
-    //                 done();
-    //             });
-    //     });
 });
